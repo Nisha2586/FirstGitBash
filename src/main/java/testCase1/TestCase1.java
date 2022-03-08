fc3cdd5dcdd42b7ae314447dbe4be997bba37e97
Nisha2586 <nisham2586@gmail.com>
2022-03-08 11:40:24 -0500

    Added Testcase2

--- src/main/java/testCase1/TestCase1.java
+++ src/main/java/testCase1/TestCase1.java
@@ -3,8 +3,9 @@ package testCase1;
 public class TestCase1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		System.out.println("TestCase 1");
+		System.out.println("Test Case 2");
 	}
 
 }
